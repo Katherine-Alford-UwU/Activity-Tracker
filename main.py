fea3b1756462737742753e96c37179d827a61e1f
Katherine-Alford-UwU <99621143+Katherine-Alford-UwU@users.noreply.github.com>
2022-03-13 16:57:22 -0400

    fix'd excel
    yay

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-from Tracker import StepTracker,  SleepTracker, WeatherTracker
+from openpyxl import Workbook, load_workbook
+from Tracker import StepTracker, SleepTracker, WeatherTracker
 
-track = WeatherTracker("Katherine", 12)
-track.addDegree(42, "3/2/22")
-track.addDegree(13, "1/5/22")
-track.addDegree(90, "12/31/22")
-print(track.degreeList)
-print(track.dateList)
+workbook = load_workbook("trackerdata.xlsx")
+sheet = workbook.get_sheet_by_name('Sheet1')
 
-print(track.getDegree("12/31/22"))
+track = WeatherTracker("Katherine",12)
+track2 = SleepTracker("Katherine",12)
+track3 = StepTracker("Katherine", 12)
 
-#[5,17,9]
-#[30, 32, 23]
+date1 = input("What is the date? MM/DD/YY")
+watHrs = input("How many hours did you sleep last night?")
+watSteps = int(input("How many steps did you take today?"))
+watWethur = input("What was the high and low of today's weather? low/high")
 
-#0
-#track.printEnoughSteps()
+workbookRead = load_workbook ('trackerdata.xlsx')
+sheetName = workbookRead['Sheet1']
+
+emptyRow = 0
+for i in range (1,1000000):
+    if (sheetName['A' + str(i)].value == None):
+        emptyRow = i
+        break
+
+sheet.cell(column=1, row=emptyRow, value=date1)
+sheet.cell(column=2,row=emptyRow, value=watSteps)
+sheet.cell(column=3,row=emptyRow, value=watHrs)
+sheet.cell(column=4,row=emptyRow, value=watWethur)
+
+workbook.save(filename="trackerdata.xlsx")
